7b7552ff0acc24ec09718f746de792f3354ac3c6
mdmontes <manuel.montes0@gmail.com>
2023-03-06 09:32:58 -0500

    answers for lesson 9

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 require('dotenv').config();
 require('express-async-errors');
+
+
 const express = require('express');
 const app = express();
+
+const connectDB = require('./db/connect');
+const authenticate = require('./middleware/authentication');
+
+
+const authrouter = require('./routes/auth')
+const productsrouter = require('./routes/products')
+
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
@@ -10,10 +20,14 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 app.use(express.json());
 // extra packages
 
+
+// app.get('/', (req, res) => {
+//   res.send('jobs api');
+// });
+
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use('/api/v1/auth', authrouter);
+app.use('/api/v1/products', authenticate , productsrouter); 
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -22,6 +36,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
---
+++ controllers/auth.js
@@ -0,0 +1,35 @@
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, UnauthenticatedError } = require('../errors')
+
+const register = async(req, res)=>{
+  // const {name, email ,password} = req.body
+  const user = await User.create(req.body)
+  const token = user.createJWT()
+  res.status(StatusCodes.CREATED).json({msg: `the user ${user.name} was created`, token});
+  ;
+}
+
+const login = async(req, res)=>{
+  const {email ,password} = req.body
+
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password')
+  }
+
+  const user = await User.findOne({email})
+
+  const isPasswordCorrect = await user.comparePassword(password)
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('Invalid Credentials')
+  }
+
+  const token = user.createJWT()
+  res.status(StatusCodes.CREATED).json({msg: `the user ${user.name} has logged on`, token});
+}
+
+
+module.exports = {
+  register,
+  login
+}
